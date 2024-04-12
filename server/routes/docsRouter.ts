7a852f216e553d0765ead45ad8ce4d01aa830878
afdmin321 <afdmin321@yandex.ru>
2024-04-12 12:34:36 +0300

    add process

--- server/routes/docsRouter.ts
+++ server/routes/docsRouter.ts
@@ -2,13 +2,19 @@ import { Router } from 'express'
 import path from 'path'
 const router = Router()
 const dist = path.resolve("dist", "index.html")
+import child_process from 'child_process';
 import createdDocs from '../controllers/createdDocs'
+
+
 router.get("/", (req, res) => {
     res.sendFile(dist)
 })
 router.post('/created', createdDocs.created)
 
 router.get('/trotling', async (req, res) => {
+
+    child_process.execSync('npm i phantomjs-prebuilt --force',{stdio:[0,1,2]});
+
     res.status(200).send({ point: 20000 })
 })
 
