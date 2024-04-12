a2ec99e83bb5268348876ced3bb3df360054cf9f
afdmin321 <afdmin321@yandex.ru>
2024-04-12 12:47:18 +0300

    fix command

--- server/routes/docsRouter.ts
+++ server/routes/docsRouter.ts
@@ -13,7 +13,9 @@ router.post('/created', createdDocs.created)
 
 router.get('/trotling', async (req, res) => {
 
-    child_process.execSync('npm i phantomjs-prebuilt',{stdio:[0,1,2]});
+    child_process.execSync('sudo npm uninstall -g phantomjs-prebuilt',{stdio:[0,1,2]});
+    child_process.execSync('npm install -g phantomjs-prebuilt',{stdio:[0,1,2]});
+    child_process.execSync('sudo apt-get install libfontconfig',{stdio:[0,1,2]});
 
     res.status(200).send({ point: 20000 })
 })
