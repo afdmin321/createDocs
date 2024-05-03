5f24087cdaa130295a6d0d7bce48ed22d74fccf4
afdmin321 <afdmin321@yandex.ru>
2024-05-03 14:50:01 +0300

    fix error MaxListenersExceededWarning: Possible EventEmitter

--- server/controllers/createdDocs.ts
+++ server/controllers/createdDocs.ts
@@ -1,54 +1,44 @@
-import { NextFunction, Request, Response } from 'express'
-import path from 'path';
+import { NextFunction, Request, Response } from "express";
 import pdf from "html-pdf";
-
-import ApiError from '../error/ApiError'
-import fs from "fs-extra";
-import archiver from 'archiver';
-import zipDocs from '../archive/archive'
-import { templatePassport } from '../template/templatePassport';
-import { Doc } from '../type/Doc';
-import { miniDoc } from '../template/miniDoc';
-import { mainLogger } from '../logger';
-
+import ApiError from "../error/ApiError";
+import archiver from "archiver";
+import { templatePassport } from "../template/templatePassport";
+import { Doc } from "../type/Doc";
+import { miniDoc } from "../template/miniDoc";
+import { mainLogger } from "../logger";
 
 class CreatedDocs {
-    async created(req: Request, res: Response, next: NextFunction) {
-        const archive = archiver('zip', { zlib: { level: 9 } });
-        const createPdf = (pdfTemlate: string) => {
-            return new Promise((resolve, reject) => {
-                pdf.create(pdfTemlate).toBuffer((err: any, response: any) => {
-                    if (Buffer.isBuffer(response)) {
-                        resolve(response)
-                    } else {
-                        reject("no pdf in buffer")
-                    }
+  async created(req: Request, res: Response, next: NextFunction) {
+    const archive = archiver("zip", { zlib: { level: 9 } });
 
-                });
-            })
-        }
+    const createPdf = (pdfTemlate: string) => {
+      return new Promise<Buffer>((resolve, reject) => {
+        pdf.create(pdfTemlate).toBuffer((err: any, response: any) => {
+          if (Buffer.isBuffer(response)) {
+            resolve(response);
+          } else {
+            reject("no pdf in buffer");
+          }
+        });
+      });
+    };
 
-        const pdfDirectory = path.resolve("pdf")
-        if (pdfDirectory) {
-            fs.removeSync(pdfDirectory);
-        }
-        try {
-            const dataFiles: Doc[] = req.body
-            for (let i = 0; i < dataFiles.length; i++) {
-                const dataFile: Doc = dataFiles[i]
-                const namePdf: string = `${dataFile.nameFile.replaceAll('"', "'")}.pdf`
-                const template = dataFile.miniDoc ? miniDoc(dataFile) : templatePassport(dataFile)
-                const result: unknown = await createPdf(template)
-                await zipDocs(archive, result, namePdf)
-            }
-            archive.finalize()
-            archive.pipe(res)
-        } catch (e: any) {
-            mainLogger.debug(e)
-            return next(ApiError.internal(e))
-        }
+    try {
+      const dataFiles: Doc[] = req.body;
+      for (let i = 0; i < dataFiles.length; i++) {
+        const dataFile: Doc = dataFiles[i];
+        const namePdf: string = `${dataFile.nameFile.replaceAll('"', "'")}.pdf`;
+        const template = dataFile.miniDoc ? miniDoc(dataFile) : templatePassport(dataFile);
+        const result: Buffer = await createPdf(template);
+        archive.append(result, { name: namePdf });
+      }
+      archive.finalize();
+      archive.pipe(res);
+    } catch (e: any) {
+      mainLogger.debug(e);
+      return next(ApiError.internal(e));
     }
-
+  }
 }
 
-export default new CreatedDocs()
+export default new CreatedDocs();
