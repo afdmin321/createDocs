191ba97ad36acef621c9637d874d6b7913550b24
afdmin321 <afdmin321@yandex.ru>
2024-05-20 16:47:32 +0300

    refact

--- client/src/features/downloadDocs/model/services/dowmloadDocs.ts
+++ client/src/features/downloadDocs/model/services/dowmloadDocs.ts
@@ -2,40 +2,39 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import { StateSchema } from "app/providers/StoreProvider";
 import axios from "axios";
 import { getCards } from "entities/Cards/model/selectors/getCards";
-import { URL_ADDRESS } from "shared/const/const";
+import { URL_ADDRESS, localhost } from "shared/const/const";
+
 // @ts-ignore
-const jszip = require('jszip')
+const jszip = require("jszip");
 
 interface config {
-    rejectValue: string;
-    state: StateSchema;
-
+  rejectValue: string;
+  state: StateSchema;
 }
 export const downloadDocs = createAsyncThunk<any, void, config>(
-    "login/loginByUsername",
-    async (_, thunkAPi) => {
-        const { rejectWithValue, getState } = thunkAPi;
-        const docs = getCards(getState())
-        try {
-            const response = await axios.post(`${URL_ADDRESS}/created`, docs, { responseType: 'blob' })
+  "login/loginByUsername",
+  async (_, thunkAPi) => {
+    const { rejectWithValue, getState } = thunkAPi;
+    const docs = getCards(getState());
+    try {
+      const response = await axios.post(`${localhost}created`, docs, { responseType: "blob" });
 
-            if (!response) {
-                throw new Error();
-            }
-            const zipBlob = await response.data;
+      if (!response) {
+        throw new Error();
+      }
+      const zipBlob = await response.data;
 
-            const zip = await jszip.loadAsync(zipBlob); // Manipulate the zip content if required 
-            await zip.generateAsync({ type: 'blob' })
-                .then((content: any) => {
-                    const url = URL.createObjectURL(content);
-                    const link = document.createElement('a');
-                    link.href = url; link.download = 'filename.zip';
-                    link.click();
-                    URL.revokeObjectURL(url);
-                });
-
-        } catch (err) {
-            return rejectWithValue("error");
-        }
+      const zip = await jszip.loadAsync(zipBlob); // Manipulate the zip content if required
+      await zip.generateAsync({ type: "blob" }).then((content: any) => {
+        const url = URL.createObjectURL(content);
+        const link = document.createElement("a");
+        link.href = url;
+        link.download = "filename.zip";
+        link.click();
+        URL.revokeObjectURL(url);
+      });
+    } catch (err) {
+      return rejectWithValue("error");
     }
+  }
 );
